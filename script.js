334b5735fca07c73c4d0b61d0d83d9ad49f6c8ff
aisha-06 <ayesha140406@gmail.com>
2024-09-13 14:07:01 +0530

    home page done!!

---
+++ script.js
@@ -0,0 +1,321 @@
+function mode_light()
+{
+    var body=document.querySelector('.body');
+    body.style.backgroundColor='white';
+    var call=document.querySelector('.btn-call');
+    call.style.backgroundColor='white';
+
+    var headerSocial=document.querySelector('.header-item-social');
+    headerSocial.style.color='black';
+    headerSocial.style.backgroundColor='white';
+
+    var order=document.querySelector('.order-btn');
+    order.style.backgroundColor='white';
+
+    //sticky header part
+    var stick=document.querySelector('.stick-header');
+    stick.style.backgroundColor='white';
+
+    var home=document.querySelector('.footer-btn-home');
+    home.style.color='black';
+    home.style.backgroundColor='white';
+
+    var cake=document.querySelector('.footer-btn-cake');
+    cake.style.color='black';
+    cake.style.backgroundColor='white';
+
+    var about=document.querySelector('.footer-btn-about');
+    about.style.color='black';
+    about.style.backgroundColor='white';
+
+    var blog=document.querySelector('.footer-btn-blog');
+    blog.style.color='black';
+    blog.style.backgroundColor='white';
+
+    var contact=document.querySelector('.footer-btn-contact');
+    contact.style.color='black';
+    contact.style.backgroundColor='white';
+    
+    
+    var cake=document.querySelector('.mode-btn');
+    cake.style.color='black';
+    cake.style.backgroundColor='white';
+
+    var schilers=document.querySelector('.schilers-text');
+    schilers.style.color='black';
+    
+    var schilersPara=document.querySelector('.schilers-para');
+    schilersPara.style.color='black';
+    
+    var schilersPara=document.querySelector('.schilers-para2');
+    schilersPara.style.color='black';
+
+    var delightful=document.querySelector('.cake-delightful-text');
+    delightful.style.color='black';
+
+    var cashmere=document.querySelector('.cake-cashmere-text');
+    cashmere.style.color='black';
+
+    var suprise=document.querySelector('.cake-suprise-text');
+    suprise.style.color='black';
+
+    var review1=document.querySelector('.para-review1');
+    review1.style.color='black';
+
+    var review2=document.querySelector('.para-review2');
+    review2.style.color='black';
+    
+    var review3=document.querySelector('.para-review3');
+    review3.style.color='black';
+
+    var cust1=document.querySelector('.cust-name1');
+    cust1.style.color='black';
+
+    var cust2=document.querySelector('.cust-name2');
+    cust2.style.color='black';
+
+    var cust3=document.querySelector('.cust-name3');
+    cust3.style.color='black';
+    
+    var blogBtn=document.querySelector('.blog-btn');
+    blogBtn.style.backgroundColor='white';
+
+    var blogtxt=document.querySelector('.blog-text');
+    blogtxt.style.color='black';
+
+    var recipara=document.querySelector('.recipe-para');
+    recipara.style.color='black';
+
+    var recipara=document.querySelector('.recipe-para2');
+    recipara.style.color='black';
+
+    var follow=document.querySelector('.follow-text');
+    follow.style.color='black';
+
+    var followBtn=document.querySelector('.follow-btn');
+    followBtn.style.backgroundColor='white';
+
+
+    var home2=document.querySelector('.home');
+    home2.style.color='black';
+    home2.style.backgroundColor='white';
+
+    var cake2=document.querySelector('.cake');
+    cake2.style.color='black';
+    cake2.style.backgroundColor='white';
+
+    var about2=document.querySelector('.about');
+    about2.style.color='black';
+    about2.style.backgroundColor='white';
+
+    var blog2=document.querySelector('.blog');
+    blog2.style.color='black';
+    blog2.style.backgroundColor='white';
+
+    var contact2=document.querySelector('.contact');
+    contact2.style.color='black';
+    contact2.style.backgroundColor='white';
+      
+    var footerpara=document.querySelector('.contact-footer-para');
+    footerpara.style.color='black';
+
+    var socialfooter=document.querySelector('.btn-social-footer');
+    socialfooter.style.backgroundColor='white';
+    socialfooter.style.color='black';
+  
+    var socialfooter2=document.querySelector('.footer2');
+    socialfooter2.style.backgroundColor='white';
+    socialfooter2.style.color='black';
+
+    var socialfooter3=document.querySelector('.footer3');
+    socialfooter3.style.backgroundColor='white';
+    socialfooter3.style.color='black';
+
+    var locIcon=document.querySelector('.loc-icon');
+    locIcon.style.color='black';
+
+    var loctxt=document.querySelector('.loc-text');
+    loctxt.style.color='black';
+
+    var contactIcon=document.querySelector('.contact-icon');
+    contactIcon.style.color='black';
+
+    var contacttxt=document.querySelector('.contact-text');
+    contacttxt.style.color='black';
+
+    var footerCall=document.querySelector('.footer-calling-no');
+    footerCall.style.backgroundColor='white';
+    footerCall.style.color='black';
+
+    var lasttxt=document.querySelector('.last-footer-text');
+    lasttxt.style.color='black';
+
+    var heart=document.querySelector('.heart');
+    heart.style.color='red';
+
+    return false;
+    
+    
+    
+}
+function mode_dark()
+{
+    var body=document.querySelector('.body');
+    body.style.backgroundColor='black';
+    var call=document.querySelector('.btn-call');
+    call.style.backgroundColor='black';
+
+    var headerSocial=document.querySelector('.header-item-social');
+    headerSocial.style.color='white';
+    headerSocial.style.backgroundColor='black';
+
+    var order=document.querySelector('.order-btn');
+    order.style.backgroundColor='gold';
+    order.style.color='black';
+
+    var cake=document.querySelector('.light-btn');
+    cake.style.color='white';
+    cake.style.backgroundColor='black';
+
+    //sticky header part
+    var stick=document.querySelector('.stick-header');
+    stick.style.backgroundColor='black';
+
+    var home=document.querySelector('.footer-btn-home');
+    home.style.color='white';
+    home.style.backgroundColor='black';
+
+    var cake=document.querySelector('.footer-btn-cake');
+    cake.style.color='white';
+    cake.style.backgroundColor='black';
+
+    var about=document.querySelector('.footer-btn-about');
+    about.style.color='white';
+    about.style.backgroundColor='black';
+
+    var blog=document.querySelector('.footer-btn-blog');
+    blog.style.color='white';
+    blog.style.backgroundColor='black';
+
+    var contact=document.querySelector('.footer-btn-contact');
+    contact.style.color='white';
+    contact.style.backgroundColor='black';
+
+    var schilers=document.querySelector('.schilers-text');
+    schilers.style.color='white';
+    
+    var schilersPara=document.querySelector('.schilers-para');
+    schilersPara.style.color='white';
+    
+    var schilersPara=document.querySelector('.schilers-para2');
+    schilersPara.style.color='white';
+
+    var delightful=document.querySelector('.cake-delightful-text');
+    delightful.style.color='white';
+
+    var cashmere=document.querySelector('.cake-cashmere-text');
+    cashmere.style.color='white';
+
+    var suprise=document.querySelector('.cake-suprise-text');
+    suprise.style.color='white';
+
+    var review1=document.querySelector('.para-review1');
+    review1.style.color='white';
+
+    var review2=document.querySelector('.para-review2');
+    review2.style.color='white';
+    
+    var review3=document.querySelector('.para-review3');
+    review3.style.color='white';
+
+    var cust1=document.querySelector('.cust-name1');
+    cust1.style.color='white';
+
+    var cust2=document.querySelector('.cust-name2');
+    cust2.style.color='white';
+
+    var cust3=document.querySelector('.cust-name3');
+    cust3.style.color='white';
+    
+    var blogBtn=document.querySelector('.blog-btn');
+    blogBtn.style.backgroundColor='black';
+    blogBtn.style.color='gold';
+
+    var blogtxt=document.querySelector('.blog-text');
+    blogtxt.style.color='white';
+
+    var recipara=document.querySelector('.recipe-para');
+    recipara.style.color='white';
+
+    var recipara=document.querySelector('.recipe-para2');
+    recipara.style.color='white';
+
+    var follow=document.querySelector('.follow-text');
+    follow.style.color='white';
+
+    var followBtn=document.querySelector('.follow-btn');
+    followBtn.style.backgroundColor='black';
+
+
+    var home2=document.querySelector('.home');
+    home2.style.color='white';
+    home2.style.backgroundColor='black';
+
+    var cake2=document.querySelector('.cake');
+    cake2.style.color='white';
+    cake2.style.backgroundColor='black';
+
+    var about2=document.querySelector('.about');
+    about2.style.color='white';
+    about2.style.backgroundColor='black';
+
+    var blog2=document.querySelector('.blog');
+    blog2.style.color='white';
+    blog2.style.backgroundColor='black';
+
+    var contact2=document.querySelector('.contact');
+    contact2.style.color='white';
+    contact2.style.backgroundColor='black';
+      
+    var footerpara=document.querySelector('.contact-footer-para');
+    footerpara.style.color='white';
+
+    var socialfooter=document.querySelector('.btn-social-footer');
+    socialfooter.style.backgroundColor='black';
+    socialfooter.style.color='white';
+  
+    var socialfooter2=document.querySelector('.footer2');
+    socialfooter2.style.backgroundColor='black';
+    socialfooter2.style.color='white';
+
+    var socialfooter3=document.querySelector('.footer3');
+    socialfooter3.style.backgroundColor='black';
+    socialfooter3.style.color='white';
+
+    var locIcon=document.querySelector('.loc-icon');
+    locIcon.style.color='white';
+
+    var loctxt=document.querySelector('.loc-text');
+    loctxt.style.color='white';
+
+    var contactIcon=document.querySelector('.contact-icon');
+    contactIcon.style.color='white';
+
+    var contacttxt=document.querySelector('.contact-text');
+    contacttxt.style.color='white';
+
+    var footerCall=document.querySelector('.footer-calling-no');
+    footerCall.style.backgroundColor='black';
+    footerCall.style.color='white';
+
+    var lasttxt=document.querySelector('.last-footer-text');
+    lasttxt.style.color='white';
+
+    var heart=document.querySelector('.heart');
+    heart.style.color='gold';
+
+    return false;
+    
+    
+    
+}
